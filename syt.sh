01782f0c3132b3eef142e1ae9afb3ae5a9c0e7bf
Sxmon17 <simonguglberger@gmail.com>
2022-10-14 18:45:55 +0200

    added flags and more arguments

--- syt.sh
+++ syt.sh
@@ -1,19 +1,42 @@
 #!/bin/sh
+ARGS=$(getopt -a --options e:s:t: --long "exercise:,subject:,title:" -- "$@")
+eval set -- "$ARGS"
+EXERCISE=""
+SUBJECT=""
+TITLE=""
 
-ZIPFILE=$(ls $HOME/Downloads/ | grep "UE$1")
+while true; do
+  case "$1" in
+    -e|--exercise)
+      EXERCISE="$2"
+      shift 2;;
+    -s|--subject)
+      SUBJECT="$2"
+      shift 2;;
+    -t|--title)
+      TITLE="$2"
+      shift 2;;
+    --)
+      break;;
+     *)
+      printf "Unknown option %s\n" "$1"
+      exit 1;;
+  esac
+done
+
+ZIPFILE=$(ls $HOME/Downloads/ | grep "UE$EXERCISE")
 
 if test -z "$ZIPFILE"
 then
 	echo "error: exercisefile not found"
 	exit 1
 else
-	echo "Located Exercise: $ZIPFILE"
+	echo "located exercise: $ZIPFILE"
 fi
 
-SUBJECT=$2
-
+echo "moving $ZIPFILE to $HOME/School"
 mv "$HOME/Downloads/$ZIPFILE" "$HOME/School/"
-echo "Moved $ZIPFILE to $HOME/School"
-unzip "$HOME/School/$ZIPFILE" -d "$HOME/School/$SUBJECT/UE$1"
-
-
+echo "unzipping: $HOME/School/$ZIPFILE" 
+unzip "$HOME/School/$ZIPFILE" -d "$HOME/School/$SUBJECT/UE${EXERCISE}_${TITLE}"
+echo "deleting: $HOME/School/$ZIPFILE"
+rm -rf "$HOME/School/$ZIPFILE"
